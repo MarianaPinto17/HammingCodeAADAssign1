205ff3b800ccf32eed11862fc59ccfd63d3bf5f5
HugoMoinheiro <hugo.moinheiro@ua.pt>
2021-12-05 15:32:28 +0000

    Update decoder parallel

--- Parallel_Decoder/decoder.vhd
+++ Parallel_Decoder/decoder.vhd
@@ -15,6 +15,8 @@ architecture structural of decoder is
 
 	signal xor_a, xor_b		: std_logic;
 	signal p1, p2, p3, p4	: std_logic;
+	signal H1, H2, H3, H4 	: std_logic_vector(10 downto 0);
+	signal word, parWord		: std_logic_vector(3 downto 0);
 	component xor4
 		port (x0, x1, x2, x3	: in std_logic;
 				y					: out std_logic);
@@ -25,10 +27,27 @@ architecture structural of decoder is
 	end component;
 	
 begin
+	H1 <= "11100011101";
+	H2 <= "10011011011";
+	H3 <= "01010110111";
+	H4 <= "00101101111";
+
 	xorA:	xor4 port map (c(0), c(6), c(7), c(10), xor_a);
 	xorB:	xor4 port map (c(5), c(8), c(9), c(10), xor_b);
 	par1:	xor5 port map (c(1), c(2), c(8), c(11), xor_a, p1);
 	par2:	xor5 port map (c(3), c(4), c(9), c(12), xor_a, p2);
 	par3: xor5 port map (c(1), c(3), c(6), c(13), xor_b, p3);
 	par4: xor5 port map (c(2), c(4), c(7), c(14), xor_b, p4);
+	parWord <= par1 & par2 & par3 & par4;
+	
+	for i in 0 to 10 loop
+	begin
+		word <= H1(i) & H2(i) & H3(i) & H4(i);
+		if (word = parWord) then
+			d(i) := c(i) + "1";
+		else 
+			d(i) := c(i);
+		end if;
+	end loop;
+	
 end structural;
